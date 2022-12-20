d99dd422c4dbc04b2ac0f3acbc6d211fcac958a5
JunaidKhan8855 <junaid.khan@digitify.com>
2022-12-21 02:22:50 +0500

    Test case added for language value when initilized null

--- SadaPayHomeAssignmentTests/Test Models/TrendingItemModelTests.swift
+++ SadaPayHomeAssignmentTests/Test Models/TrendingItemModelTests.swift
@@ -17,6 +17,18 @@ final class TrendingItemModelTests: XCTestCase {
         XCTAssertEqual(sut.repoDescription, repoDescription)
         XCTAssertEqual(sut.language, language)
         XCTAssertEqual(sut.starCount, starCount)
+    }
 
+    func test_WhenInitilized_withNullLanguageProperty_ThenReturnsLanguagePropertyNull() {
+        let name = "swift"
+        let repoDescription = "The Swift Programming Language"
+        let starCount = 514
+        let sut = TrendingItemModel(name: name,
+                                    repoDescription:repoDescription,
+                                    starCount: starCount)
+        XCTAssertEqual(sut.name, name)
+        XCTAssertEqual(sut.repoDescription, repoDescription)
+        XCTAssertNil(sut.language)
+        XCTAssertEqual(sut.starCount, starCount)
     }
 }
