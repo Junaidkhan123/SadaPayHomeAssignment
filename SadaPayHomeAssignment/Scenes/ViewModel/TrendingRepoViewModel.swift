ef188070849caa35262ae3ff6246c17162c25c60
JunaidKhan8855 <junaid.khan@digitify.com>
2022-12-24 21:43:54 +0500

    Add more tests and add TrendingRepoViewModelType

--- SadaPayHomeAssignment/Scenes/ViewModel/TrendingRepoViewModel.swift
+++ SadaPayHomeAssignment/Scenes/ViewModel/TrendingRepoViewModel.swift
@@ -6,7 +6,32 @@
 //
 
 import Foundation
-final class TrendingRepoViewModel {
+
+protocol TrendingRepoViewModelType {
+    var title : String { get }
+    var trendingRepo: [TrendingItemModel] { get }
+    func fetchTrendingRepositories(completionHanlder: @escaping ([TrendingItemModel]?) -> Void)
+}
+
+final class TrendingRepoViewModel: TrendingRepoViewModelType {
     var title: String { return  "Trending" }
     private(set) var trendingRepo: [TrendingItemModel] = []
+
+    var service: APIServiceProtocol
+
+    init(service : APIServiceProtocol = APIService()) {
+        self.service = service
+    }
+
+    func fetchTrendingRepositories(completionHanlder: @escaping ([TrendingItemModel]?) -> Void) {
+        service.fetchTrendingRepos {[weak self] result in
+            switch result {
+            case .success(let trendingRepo):
+                self?.trendingRepo = trendingRepo.count > 0 ? trendingRepo : []
+                completionHanlder(trendingRepo)
+            case .failure(_):
+                completionHanlder(nil)
+            }
+        }
+    }
 }
--- SadaPayHomeAssignmentTests/Test ViewModel/TrendingRepoViewModelTests.swift
+++ SadaPayHomeAssignmentTests/Test ViewModel/TrendingRepoViewModelTests.swift
@@ -3,15 +3,55 @@ import XCTest
 
 final class TrendingRepoViewModelTests: XCTestCase {
 
+    var sut: TrendingRepoViewModel!
+
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+        sut = TrendingRepoViewModel(service: MockSuccessAPIService())
+    }
+
+    override func tearDownWithError() throws {
+        sut = nil
+        try super.tearDownWithError()
+    }
+
     func test_WhenInitiliazed_ThenTitleMatchWithResultValue() {
-        let sut = TrendingRepoViewModel()
         let title = "Trending"
         XCTAssertEqual(sut.title, title)
     }
 
-    func test_WhenInitilized_ThenTrendingItemCountIsZero() {
-        let sut = TrendingRepoViewModel()
+    func test_WhenInitilized_ThenTrendingItemResultCountIsZero() {
         let repo = sut.trendingRepo
         XCTAssertEqual(repo.count, 0 )
     }
+
+    func test_WhenFetchTrendingRepositoriesInvokedWithSucess_ThenTrendingItemResultCountIsNotNil() {
+
+        let successExpectations = expectation(description: "Success Expectations for trendingRepositories")
+        let stub: [TrendingItemModel] = TrendingItemResult.stub.items
+        var outputRepositories: [TrendingItemModel]?
+        sut.fetchTrendingRepositories { trendingRepo in
+            successExpectations.fulfill()
+            outputRepositories = trendingRepo
+        }
+        XCTAssertNotNil(outputRepositories)
+        XCTAssertTrue(sut.trendingRepo.count > 0)
+        XCTAssertEqual(stub, outputRepositories)
+        waitForExpectations(timeout: 1.0)
+    }
+
+    func test_WhenFetchTrendingRepositoriesInvokedWithFailure_ThenTrendingItemResultIsNil() {
+        let service = MockFailureAPIService()
+        let sut = TrendingRepoViewModel(service: service)
+        let faiulureExpectations = expectation(description: "Failure expectations for top trendingRepositories")
+
+        var outputRepositories: [TrendingItemModel]?
+        sut.fetchTrendingRepositories { articles in
+            faiulureExpectations.fulfill()
+            outputRepositories = articles
+        }
+        XCTAssertNil(outputRepositories)
+        waitForExpectations(timeout: 1.0)
+    }
+
 }
