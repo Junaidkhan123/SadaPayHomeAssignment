b49b2b382bd519c1dfb4c5a818158b1e68f8db38
JunaidKhan8855 <junaid.khan@digitify.com>
2022-12-25 20:01:41 +0500

    Add more test for tableView in RepositoriesViewController

--- SadaPayHomeAssignment/Scenes/RepositoriesViewController.swift
+++ SadaPayHomeAssignment/Scenes/RepositoriesViewController.swift
@@ -6,14 +6,21 @@
 //
 
 import UIKit
+enum Section {
+    case trendingRepositories
+}
 
 class RepositoriesViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
+
+    private lazy var dataSource = configureDataSource()
     private var viewModel: TrendingRepoViewModelType
+
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = viewModel.title
         setupTableView()
+        fetchRepositories()
     }
 
     init(viewModel: TrendingRepoViewModelType = TrendingRepoViewModel()) {
@@ -27,6 +34,34 @@ class RepositoriesViewController: UIViewController {
 
     private func setupTableView() {
         tableView.register(UINib(nibName: "RepoTableViewCell", bundle: nil), forCellReuseIdentifier: RepoTableViewCell.identifier)
+        tableView.dataSource = dataSource
     }
 
+    private func configureDataSource() -> UITableViewDiffableDataSource<Section, TrendingItemModel> {
+
+        let dataSource = UITableViewDiffableDataSource<Section, TrendingItemModel>(tableView: tableView, cellProvider: { (tableView, indexPath, trendingItemModel) -> UITableViewCell? in
+            let cell = tableView.dequeueReusableCell(withIdentifier: RepoTableViewCell.identifier, for: indexPath) as! RepoTableViewCell
+            return cell
+        })
+
+        return dataSource
+    }
+
+    private func fetchRepositories() {
+        viewModel.fetchTrendingRepositories {  [weak self] result in
+            guard let self = self else { return }
+            if result == nil {
+            }
+            else {
+                self.updateSnapShot(trendingItems: result!)
+            }
+        }
+    }
+
+    private func updateSnapShot(trendingItems: [TrendingItemModel]) {
+        var snapShot = NSDiffableDataSourceSnapshot<Section, TrendingItemModel>()
+        snapShot.appendSections([.trendingRepositories])
+        snapShot.appendItems(trendingItems)
+        dataSource.apply(snapShot)
+    }
 }
--- SadaPayHomeAssignmentTests/Test ViewControllers/RepositoriesViewControllerTests.swift
+++ SadaPayHomeAssignmentTests/Test ViewControllers/RepositoriesViewControllerTests.swift
@@ -3,23 +3,86 @@ import XCTest
 
 final class RepositoriesViewControllerTests: XCTestCase {
 
+    var sut: RepositoriesViewController!
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+        sut = makeSut()
+    }
+
+    override func tearDownWithError() throws {
+        sut = nil
+        try super.tearDownWithError()
+    }
+
     func test_WhenIntiliazed_ThenTitleShouldSets() {
-        let sut = RepositoriesViewController()
-        sut.loadViewIfNeeded()
         XCTAssertEqual(sut.title, "Trending")
     }
 
     // MARK: - TableView Tests
     func test_WhenControllerLoaded_ThenControllerHasTableView() {
-        let sut = RepositoriesViewController()
-        sut.loadViewIfNeeded()
         XCTAssertNotNil(sut.tableView)
     }
 
-    func test_whenTableViewIsConfigured_ThenTableViewHasCell() {
-        let sut = RepositoriesViewController()
-        sut.loadViewIfNeeded()
+    func test_WhenTableViewIsConfigured_ThenTableViewHasCell() {
         let cell = sut.tableView.dequeueReusableCell(withIdentifier: RepoTableViewCell.identifier, for: IndexPath(item: 0, section: 0))
         XCTAssertNotNil(cell)
+    }
+
+    func test_WhenTableViewIsConfigured_ThenRepoTableViewCellReturns() throws {
+        let tableView = try XCTUnwrap(sut.tableView)
+        let indexPath = IndexPath(row: 0, section: 0)
+        let cell = try XCTUnwrap(
+            tableView.dataSource?
+                .tableView(tableView,
+                           cellForRowAt: indexPath)
+            as? RepoTableViewCell
+        )
+        XCTAssertNotNil(cell)
+    }
+
+    func test_WhenDataIsFetchedSuccessFully_ThenReturnsCorrectNumberOfRows() throws {
+        let mockSuccessViewModel = MockSuccessTrendingViewModel()
+        let sut = makeSut(viewModel: mockSuccessViewModel)
+        
+        mockSuccessViewModel.fetchTrendingRepositories { repositories in
+            XCTAssertEqual(sut.tableView.numberOfRows(inSection: 0), repositories?.count)
+        }
+    }
+
+    func test_WhenDataFetchingFailed_ThenItReturnsNilRepositories() throws {
+        let mockFailureViewModel = MockFailureTrendingViewModel()
+        let _ = makeSut(viewModel: mockFailureViewModel)
+
+        mockFailureViewModel.fetchTrendingRepositories { repositories in
+            XCTAssertNil(repositories)
+        }
+    }
+
+    // MARK: - Helper
+    func makeSut(viewModel: TrendingRepoViewModelType = MockSuccessTrendingViewModel()) -> RepositoriesViewController {
+        let sut = RepositoriesViewController(viewModel: viewModel)
+        sut.loadViewIfNeeded()
+        return sut
+    }
+
+}
+
+class MockSuccessTrendingViewModel: TrendingRepoViewModelType {
+    var title: String { return "Trending"}
+
+    var trendingRepo: [TrendingItemModel] = []
+
+    func fetchTrendingRepositories(completionHanlder: @escaping ([TrendingItemModel]?) -> Void) {
+        trendingRepo = TrendingItemResult.stub.items
+        completionHanlder(TrendingItemResult.stub.items)
+    }
+}
+class MockFailureTrendingViewModel: TrendingRepoViewModelType {
+    var title: String { return "Trending"}
+
+    var trendingRepo: [SadaPayHomeAssignment.TrendingItemModel] = []
+
+    func fetchTrendingRepositories(completionHanlder: @escaping ([TrendingItemModel]?) -> Void) {
+        completionHanlder(nil)
     }
 }
